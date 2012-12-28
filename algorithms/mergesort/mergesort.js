7fc0dc153430d229cfc95d3d1469c31180cc5abf
Rashad Russell <rashad.j.russell@gmail.com>
2012-12-27 22:19:22 -0600

    Commit

---
+++ algorithms/mergesort/mergesort.js
@@ -0,0 +1,38 @@
+function mergesort(arr) {
+
+	var size = arr.length;
+	if(size < 2) {
+		return arr;
+	}
+
+	var mid   = Math.floor(size / 2);
+	var left  = arr.slice(0, mid);
+	var right = arr.slice(mid, size);
+
+	return merge(mergesort(left), mergesort(right));
+
+}
+
+function merge(left, right) {
+
+	var result = [];
+
+	while(left.length && right.length) {
+		if(left[0] <= right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+
+	while(left.length) {
+		result.push(left.shift());
+	}
+
+	while(right.length) {
+		result.push(right.shift());
+	} 
+
+	return result;
+
+}
---
+++ algorithms/quicksort/quicksort.js
@@ -0,0 +1,61 @@
+function quicksort(arr, lo, hi) {
+
+    var index;
+
+    if (arr.length > 1) {
+
+        lo = !lo ? 0 : lo;
+        hi = !hi ? arr.length - 1 : hi;
+
+        index = partition(arr, lo, hi);
+
+        if (lo < index - 1) {
+            quicksort(arr, lo, index - 1);
+        }
+
+        if (index < hi) {
+            quicksort(arr, index, hi);
+        }
+
+    }
+
+    return arr;
+
+}
+
+function partition(arr, lo, hi) {
+
+    var pivot   = arr[Math.floor((lo + hi) / 2)],
+        leftPointer  = lo,
+        rightPointer = hi;
+
+
+    while (leftPointer <= rightPointer) {
+
+        while (arr[leftPointer] < pivot) {
+            leftPointer++;
+        }
+
+        while (arr[rightPointer] > pivot) {
+            rightPointer--;
+        }
+
+        if (leftPointer <= rightPointer) {
+            swap(arr, leftPointer, rightPointer);
+            leftPointer++;
+            rightPointer--;
+        }
+        
+    }
+
+    return leftPointer;
+
+}
+
+function swap(arr, lo, hi){
+
+    var temp = arr[lo];
+    arr[lo]  = arr[hi];
+    arr[hi]  = temp;
+
+}
